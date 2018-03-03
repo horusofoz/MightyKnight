f44d47eaeb646b051ed7f9b5860954bf1fb856b6
Jeff <horusofoz@gmail.com>
2018-03-03 19:50:41 +1030

    New platform hame.
    Created a new project that contains the player and sprite classes.

---
+++ MightyKnight/Player.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MightyKnight
+{
+    class Player
+    {
+        Sprite sprite = new Sprite();
+
+        public Player()
+        {
+        }
+
+        public void Load(ContentManager content)
+        {
+            sprite.Load(content, "hero");
+        }
+
+        public void Update(float deltaTime)
+        {
+            sprite.Update(deltaTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            sprite.Draw(spriteBatch);
+        }
+
+    }
+}
